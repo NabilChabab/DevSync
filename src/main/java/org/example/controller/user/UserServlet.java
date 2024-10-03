c2cf2a702253c1fd100dd7e63d2ff15e0165c8de
NabilChabab <nabil.chababnabil@gmail.com>
2024-10-03 16:22:21 +0100

    commit

---
+++ src/main/java/org/example/controller/user/UserServlet.java
@@ -0,0 +1,28 @@
+package org.example.controller.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import org.example.models.User;
+import org.example.models.enums.UserRole;
+import org.example.repository.UserRepository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/user/dashboard.jsp").forward(request, response);
+    }
+
+
+}
